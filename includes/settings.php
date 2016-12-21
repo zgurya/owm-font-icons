77e0aef5fcc2b7ded8ca80da360fc9dae6eddb7b
azgur <azgur@DESKTOP-DIUBFBD>
2016-12-21 18:48:59 +0200

    Add API key form

---
+++ includes/settings.php
@@ -0,0 +1,16 @@
+<?php
+if(isset($_POST['api_key'])) update_option('owm_font_icons_api', $_POST['api_key']);
+?>
+<form method="POST">
+	<table class="form-table">
+		<tr>
+			<th scope="row">
+				<label for="api_key"><?php _e('Open Weather Map API', 'owm-ficons')?>: </label>
+			</th>
+			<td>
+				<input type="text" name="api_key" value="<?php echo esc_attr(get_option('owm_font_icons_api')); ?>" size="30"> <a href="https://openweathermap.org/appid" target="_blank"><?php _e('Get it here', 'owm-ficons')?></a>
+			</td>
+		</tr>
+	</table>
+	<input type="submit" value="Save" class="button button-primary button-large">
+</form>
