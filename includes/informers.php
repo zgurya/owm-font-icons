f6297cca1905f861383c6ccd1e4a44419e4e7d74
azgur <azgur@DESKTOP-DIUBFBD>
2016-12-21 19:10:14 +0200

    Create file

---
+++ includes/informers.php
@@ -0,0 +1,4 @@
+<?php 
+if(!get_option('owm_font_icons_api')||empty(get_option('owm_font_icons_api'))):?>
+	<?php _e('Please, enter API key before start using informers.', 'owm-ficons');?> <a href="<?php echo esc_url( get_admin_url(null, 'admin.php?page=owm_font_icons_settings') )?>"><?php _e('Enter it here.', 'owm-ficons');?></a>
+<?php endif;?>
